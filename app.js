03b4dc7dd6092dff988882691e03c2ef0bff5335
dev gojariya <devgojariya@devs-MacBook-Pro.local>
2025-08-21 15:04:10 +0530

    new app.js updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello hi ggod, world!');
+  res.send('Hello hi good, world!');
 });
 
 module.exports = app;  // Export app without starting server
