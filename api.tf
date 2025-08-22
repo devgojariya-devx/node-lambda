de91b7e8d253789eea827069f07e0cdc217aa9dc
dev gojariya <devgojariya@devs-MacBook-Pro.local>
2025-08-22 11:12:59 +0530

    ap.tf added

---
+++ api.tf
@@ -0,0 +1,38 @@
+resource "aws_apigatewayv2_api" "http_api" {
+  name = "express-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+
+  connection_type = "INTERNET"
+  integration_uri = aws_lambda_function.sample_lambda.invoke_arn
+  payload_format_version = "2.0"
+
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id = aws_apigatewayv2_api.http_api.id
+  route_key = "$default"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id = aws_apigatewayv2_api.http_api.id
+  name = "example-stage"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sample_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+output "api_endpoint" {
+  value = aws_apigatewayv2_stage.default.invoke_url
+}
